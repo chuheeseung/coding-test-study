da8f13c8c4c06497f452951afca3c6b68a02998c
chuheeseung <youverygood@naver.com>
2023-01-30 15:46:17 +0900

    23.01.30

---
+++ programmers/전화번호목록.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+
+    for i in range(len(phone_book)-1):
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+                answer = False
+                break
+
+    return answer
+
+# phone_book = ["119", "97674223", "1195524421"]
+phone_book = ["123","456","789"]
+# phone_book = ["12","123","1235","567","88"]
+print(solution(phone_book))
