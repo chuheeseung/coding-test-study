97bef69f348a986b323418d63c3017f1e7416337
chuheeseung <youverygood@naver.com>
2023-04-21 13:08:39 +0900

    23.04.21

---
+++ baekjoon/2606_바이러스.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n = int(input())
+v= int(input())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+
+for i in range(v):
+    a, b = map(int, input().split())
+    graph[a] += [b]
+    graph[b] += [a]
+
+visited[1] = 1
+q = deque([1])
+
+while q:
+    c = q.popleft()
+
+    for nx in graph[c]:
+        if visited[nx] == 0:
+            q.append(nx)
+            visited[nx] = 1
+
+print(sum(visited) - 1)
