119050d53c39dae601ac06d74c0c92672fa4c453
chuheeseung <youverygood@naver.com>
2023-05-05 22:12:17 +0900

    23.05.05

--- SWEA/1859_백만장자프로젝트.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-
-for _ in range(t):
-    n = int(input())
-    prices = list(map(int, input().split()))
-    result = 0
-    max_num = 0
-
-    for i in range(len(prices)-1, -1, -1):
---
+++ swea/1859_백만장자프로젝트.py
@@ -0,0 +1,9 @@
+# t = int(input())
+#
+# for _ in range(t):
+#     n = int(input())
+#     prices = list(map(int, input().split()))
+#     result = 0
+#     max_num = 0
+#
+#     for i in range(len(prices)-1, -1, -1):
