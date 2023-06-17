914c5cfccbd086c228cfaecb95a877c3e651f0fd
chuheeseung <youverygood@naver.com>
2023-06-17 14:18:04 +0900

    23.06.17

--- baekjoon/18311_왕복.py
+++ baekjoon/18311_왕복.py
@@ -1,16 +1,14 @@
 n, k = map(int, input().split())
-distances = list(map(int, input().split()))
+m = list(map(int, input().split()))
+m = m + m[::-1]
 
-for i in range(n):
-    k -= distances[i]
+for i in range(n * 2):
+    k -= m[i]
 
     if k < 0:
-        print(i + 1)
-        break
-else:
-    for i in range(n-1, -1, -1):
-        k -= distances[i]
-
-        if k < 0:
+        if i > n:
+            print((n * 2) - i)
+            break
+        else:
             print(i + 1)
             break
