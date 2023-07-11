09f820846daa085938f4ee62e8850828d5f74fe1
chuheeseung <youverygood@naver.com>
2023-07-11 20:54:28 +0900

    23.07.11

---
+++ baekjoon/16951_블록놀이.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+n_list = list(map(int, input().split()))
+min_cnt = 1001
+
+def check(i):
+    cnt = 0
+
+    for a in range(n):
+        if a < i:
+            if n_list[a] != (n_list[i] - (i - a) * k):
+                cnt += 1
+        elif a > i:
+            if n_list[a] != (n_list[i] + (a - i) * k):
+                cnt += 1
+
+    return cnt
+
+
+for i in range(n):
+    if n_list[i] > i * k:
+        count = check(i)
+        min_cnt = min(min_cnt, count)
+
+print(min_cnt)
